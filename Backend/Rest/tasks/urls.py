ebeed7243b0e74b70db82e9470e4b6bad6be7d27
Zain <learnerstudent001@gmail.com>
2023-12-31 17:33:38 +0500

    Authentication with JSON Web Tokens

--- Backend/Rest/tasks/urls.py
+++ Backend/Rest/tasks/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('tasks/', views.task_list, name='tasks_list'),
     path('tasks/<str:pk>/', views.task_detail, name='task_detail')
 ]
